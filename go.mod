bd6fb0c5bcc11ad54084ac9248a1f215489cebd3
satyajit nayak <satyajitn15@gmail.com>
2024-01-03 21:20:06 +0530

    add middleware.go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/satyajitnayk/csrf-security
 
 go 1.20
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/justinas/alice v1.2.0
+)
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,84 @@
 package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/justinas/alice"
+)
+
+func NewHandler() http.Handler {
+	return alice.New(recoverHandler, authHandler).ThenFunc(logicHandler)
+}
+
+// help in recovery from panic situation
+// used as middleware
+func recoverHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Panic("Recovered! Panic:%v", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
+func authHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/restricted", "/logout", "/deleteUser":
+		default:
+		}
+	}
+}
+
+func logicHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/restricted":
+	case "/login":
+		switch r.Method {
+		case "GET":
+		case "POST":
+		default:
+		}
+	case "/register":
+		switch r.Method {
+		case "GET":
+		case "POST":
+		default:
+		}
+	case "/logout":
+	case "/deleteUser":
+	default:
+	}
+}
+
+func nullifyTokenCookies(w *http.ResponseWriter, r *http.Request) {
+	authCookie := http.Cookie{
+		Name:     "AuthToken",
+		Value:    "",
+		Expires:  time.Now().Add(-1000 * time.Hour),
+		HttpOnly: true,
+	}
+	http.SetCookie(*w, &authCookie)
+
+	refreshCookie := http.Cookie{
+		Name:     "RefreshToken",
+		Value:    "",
+		Expires:  time.Now().Add(-1000 * time.Hour),
+		HttpOnly: true,
+	}
+	http.SetCookie(*w, &refreshCookie)
+}
+
+func setAuthAndRefreshCookies() {
+
+}
+
+func grabCsrfFromRequest(r *http.Request) string {
+
+}
